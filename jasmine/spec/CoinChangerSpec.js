20cd858851064a372710195ec78bbbbb720ad5ae
kayanhales <daybreakbliss@gmail.com>
2017-12-02 08:22:41 -0500

    create second branch. changed to recursion

--- jasmine/spec/CoinChangerSpec.js
+++ jasmine/spec/CoinChangerSpec.js
@@ -1,41 +1,41 @@
 describe('CoinChanger', function () {
   it ('returns 1 penny for 1 penny', function () {
-    expect(CoinChanger(1)).toEqual([1])
+    expect(CoinChanger(1, [25,10,5,1])).toEqual([1])
   })
 
   it ('returns 3 pennies for 3', function () {
-    expect(CoinChanger(3)).toEqual([1, 1, 1])
+    expect(CoinChanger(3, [25,10,5,1])).toEqual([1, 1, 1])
   })
 
   it ('returns 1 nickel for 5', function () {
-    expect(CoinChanger(5)).toEqual([5])
+    expect(CoinChanger(5, [25,10,5,1])).toEqual([5])
   })
 
   it ('returns 1 dime for 10', function () {
-    expect(CoinChanger(10)).toEqual([10])
+    expect(CoinChanger(10, [25,10,5,1])).toEqual([10])
   })
 
   it ('returns 2 dimes for 20', function () {
-    expect(CoinChanger(20)).toEqual([10, 10])
+    expect(CoinChanger(20, [25,10,5,1])).toEqual([10, 10])
   })
 
   it ('returns 1 quarter for 25', function () {
-    expect(CoinChanger(25)).toEqual([25])
+    expect(CoinChanger(25, [25,10,5,1])).toEqual([25])
   })
 
   it ('returns 2 quarters for 50', function () {
-    expect(CoinChanger(50)).toEqual([25, 25])
+    expect(CoinChanger(50, [25,10,5,1])).toEqual([25, 25])
   })
 
   it ('returns 3 quarters for 75', function () {
-    expect(CoinChanger(75)).toEqual([25, 25, 25])
+    expect(CoinChanger(75, [25,10,5,1])).toEqual([25, 25, 25])
   })
 
   it ('returns 2 dimes and 4 pennies for 24', function () {
-    expect(CoinChanger(24)).toEqual([10, 10, 1, 1, 1, 1])
+    expect(CoinChanger(24, [25,10,5,1])).toEqual([10, 10, 1, 1, 1, 1])
   })
 
   it ('returns 3 quarters, 2 dimes, and 4 pennies for 99', function () {
-    expect(CoinChanger(99)).toEqual([25, 25, 25, 10, 10, 1, 1, 1, 1])
+    expect(CoinChanger(99, [25,10,5,1])).toEqual([25, 25, 25, 10, 10, 1, 1, 1, 1])
   })
 })
--- jasmine/src/CoinChanger.js
+++ jasmine/src/CoinChanger.js
@@ -1,21 +1,13 @@
-function CoinChanger(amount) {
-  var returnAmount = [], total = 0;
-  [25, 10, 5, 1].forEach(function(coin) {
-    while (total + coin <= amount) {
-      returnAmount.push(coin);
-      total += coin;
-    }
-  });
-  return returnAmount;
-};
-
-/*function CoinChanger(amount){
-  var returnAmount = [], total = 0;
-  [25,10,5,1].forEach(function(coin){
-    if (amount >= coin){
-      returnAmount=[coin]*(amount/coin);
-    }
-    amount=amount-(coin*(amount/coin));
-  });
-  return returnAmount;
-};*/
+function CoinChanger(amount, coins) {
+  if (amount === 0) {
+      return [];
+  } else {
+      if (amount >= coins[0]) {
+          left = (amount - coins[0]);
+          return [coins[0]].concat(CoinChanger(left, coins));
+      } else {
+          coins.shift();
+          return CoinChanger(amount, coins);
+      }
+  }
+}
