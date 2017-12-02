0252b180c70c565d9767971aacb104fff3110c2f
kayanhales <daybreakbliss@gmail.com>
2017-12-02 08:45:38 -0500

    create third branch and change tests

--- jasmine/spec/CoinChangerSpec.js
+++ jasmine/spec/CoinChangerSpec.js
@@ -1,5 +1,5 @@
 describe('CoinChanger', function () {
-  it ('returns 1 penny for 1 penny', function () {
+  it ('returns 1 penny for 1', function () {
     expect(CoinChanger(1, [25,10,5,1])).toEqual([1])
   })
 
@@ -11,31 +11,59 @@ describe('CoinChanger', function () {
     expect(CoinChanger(5, [25,10,5,1])).toEqual([5])
   })
 
+  it ('returns 5 pennies for 5', function () {
+    expect(CoinChanger(5, [1])).toEqual([1,1,1,1,1])
+  })
+
   it ('returns 1 dime for 10', function () {
     expect(CoinChanger(10, [25,10,5,1])).toEqual([10])
   })
 
+  it ('returns 2 nickels for 10', function () {
+    expect(CoinChanger(10, [5,1])).toEqual([5,5])
+  })
+
   it ('returns 2 dimes for 20', function () {
-    expect(CoinChanger(20, [25,10,5,1])).toEqual([10, 10])
+    expect(CoinChanger(20, [25,10,5,1])).toEqual([10,10])
+  })
+
+  it ('returns 4 nickels for 20', function () {
+    expect(CoinChanger(20, [5,1])).toEqual([5,5,5,5])
   })
 
   it ('returns 1 quarter for 25', function () {
     expect(CoinChanger(25, [25,10,5,1])).toEqual([25])
   })
 
+  it ('returns 2 dimes and a nickel for 25', function () {
+    expect(CoinChanger(25, [10,5,1])).toEqual([10,10,5])
+  })
+
   it ('returns 2 quarters for 50', function () {
     expect(CoinChanger(50, [25,10,5,1])).toEqual([25, 25])
+  })
+
+  it ('returns 5 dimes for 50', function () {
+    expect(CoinChanger(50, [10,5,1])).toEqual([10,10,10,10,10])
   })
 
   it ('returns 3 quarters for 75', function () {
     expect(CoinChanger(75, [25,10,5,1])).toEqual([25, 25, 25])
   })
 
-  it ('returns 2 dimes and 4 pennies for 24', function () {
-    expect(CoinChanger(24, [25,10,5,1])).toEqual([10, 10, 1, 1, 1, 1])
+  it ('returns 7 dimes and a nickel for 75', function () {
+    expect(CoinChanger(75, [10,5,1])).toEqual([10,10,10,10,10,10,10,5])
   })
 
-  it ('returns 3 quarters, 2 dimes, and 4 pennies for 99', function () {
-    expect(CoinChanger(99, [25,10,5,1])).toEqual([25, 25, 25, 10, 10, 1, 1, 1, 1])
+  it ('returns 2 dimes and 2 pennies for 22', function () {
+    expect(CoinChanger(22, [25,10,5,1])).toEqual([10, 10, 1, 1])
+  })
+
+  it ('returns 5 nickels and 2 pennies for 22', function () {
+    expect(CoinChanger(22, [5,1])).toEqual([5,5,5,5, 1, 1])
+  })
+
+  it ('returns 4 quarters, 2 dimes, and 4 pennies for 124', function () {
+    expect(CoinChanger(124, [25,10,5,1])).toEqual([25, 25, 25, 25, 10, 10, 1, 1, 1, 1])
   })
 })
